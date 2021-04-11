dd1a380e0e0f78bff069a31b70a3fb9e6014485a
Victor Camacho <victor.camacho@block.one>
2021-04-11 05:17:51 -0400

    add initial database code

--- server.js
+++ server.js
@@ -25,18 +25,34 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser((user, done)=>{
-  done(null, user._id);
-});
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
 
-passport.deserializeUser((id, done) => {
-  myDataBase.findOne({_id: new ObjectID(id)}, (err, doc) => {
-    done(null, null);
+  // Be sure to change the title
+  app.route('/').get((req, res) => {
+    //Change the response to render the Pug template
+    res.render('pug', {
+      title: 'Connected to Database',
+      message: 'Please login'
+    });
   });
-});
 
-app.route('/').get((req, res) => {
-  res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'});
+  // Serialization and deserialization here...
+  passport.serializeUser((user, done)=>{
+    done(null, user._id);
+  });
+  
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({_id: new ObjectID(id)}, (err, doc) => {
+      done(null, doc);
+    });
+  });
+  
+  // Be sure to add this...
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('pug', { title: e, message: 'Unable to login' });
+  });
 });
 
 const PORT = process.env.PORT || 3000;
