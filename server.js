1ab9b2c41d9739c891666ab09b605f929dc065c8
Victor Camacho <victor.camacho@block.one>
2021-04-11 05:42:46 -0400

    add initial passport-local code

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const session = require('express-session');
 const passport = require('passport');
 const myDB = require('./connection');
 const ObjectID = require('mongodb').ObjectID;
+const LocalStrategy = require('passport-local');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
@@ -27,6 +28,18 @@ app.use(passport.session());
 
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
+
+  passport.use(new LocalStrategy(
+    function(username, password, done) {
+      myDataBase.findOne({username: username}, function(err,user){
+        console.log('User ' + username + ' attempted to log in');
+        if(err) { return done(err); }
+        if(!user) { return done(null, false); }
+        if(password !== user.password) { return done(null, false); }
+        return done(null, user);
+      })
+    }
+  ));
 
   // Be sure to change the title
   app.route('/').get((req, res) => {
