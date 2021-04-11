cbf0776643f790c252da52e784c635e2b870caff
Victor Camacho <victor.camacho@block.one>
2021-04-11 04:56:55 -0400

    fix missing app missing passport.session:

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(session({
 }));
 
 app.use(passport.initialize());
+app.use(passport.session());
 
 app.route('/').get((req, res) => {
   res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'});
