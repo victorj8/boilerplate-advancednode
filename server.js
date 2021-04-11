2d89c56aceffb5cb0b527f8de767ae6a7709c9b6
Victor Camacho <victor.camacho@block.one>
2021-04-11 05:10:39 -0400

    fix objectid code

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const myDB = require('./connection');
+const ObjectID = require('mongodb').ObjectID;
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
@@ -23,6 +24,16 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+passport.serializeUser((user, done)=>{
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  myDataBase.findOne({_id: new ObjectID(id)}, (err, doc) => {
+    done(null, null);
+  });
+});
 
 app.route('/').get((req, res) => {
   res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'});
