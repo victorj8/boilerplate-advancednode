fe03b3a7f35d97e4972d9a2c05f0fc0b0e9173ab
Victor Camacho <victor.camacho@block.one>
2021-04-12 04:17:17 -0400

    adding bcrypt

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
+const bcrypt = require('bcrypt');
 const myDB = require('./connection');
 const ObjectID = require('mongodb').ObjectID;
 const LocalStrategy = require('passport-local');
@@ -35,7 +36,9 @@ myDB(async client => {
         console.log('User ' + username + ' attempted to log in');
         if(err) { return done(err); }
         if(!user) { return done(null, false); }
-        if(password !== user.password) { return done(null, false); }
+        if(!bcrypt.compareSync(password, user.password)) { 
+          return done(null, false); 
+        }
         return done(null, user);
       })
     }
@@ -68,7 +71,7 @@ myDB(async client => {
     });
 
   app.post('/login', passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
-    res.render(process.cwd + '/views/pug/profile', {
+    res.render(process.cwd() + '/views/pug/profile', {
       user: req.user
     });
   });
@@ -81,9 +84,10 @@ myDB(async client => {
         }else if (user){
           res.redirect('/');
         }else{
+          const hash = bcrypt.hashSync(req.body.password, 12);
           myDataBase.insertOne({
             username: req.body.username,
-            password: req.body.password
+            password: hash
           }, (err, doc) => {
             if(err){
               res.redirect('/');
