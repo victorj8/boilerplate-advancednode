d49581f891aadc404ca8cdc6efb5d23da7c3f179
Victor Camacho <victor.camacho@block.one>
2021-04-12 05:08:33 -0400

    add github auth routes

--- routes.js
+++ routes.js
@@ -15,7 +15,8 @@ module.exports = function (app, myDataBase) {
       title: 'Connected to Database',
       message: 'Please login',
       showLogin: true,
-      showRegistration: true
+      showRegistration: true,
+      showSocialAuth: true
     });
   });
 
@@ -64,5 +65,14 @@ module.exports = function (app, myDataBase) {
     .get((req, res) => {
       req.logout();
       res.redirect('/');
+    });
+
+  app.route('/auth/github')
+    .get(passport.authenticate('github'));
+
+  app.route('/auth/github/callback')
+    .get(passport.authenticate('github', {failureRedirect: '/'}),
+    (req, res) => {
+      res.redirect('/profile');
     });
 }
