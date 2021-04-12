64a35bdd6ec44f29895dde4ebd393df6237c5251
Victor Camacho <victor.camacho@block.one>
2021-04-12 04:07:38 -0400

    added register route

--- server.js
+++ server.js
@@ -54,14 +54,15 @@ myDB(async client => {
     res.render('pug', {
       title: 'Connected to Database',
       message: 'Please login',
-      showLogin: true
+      showLogin: true,
+      showRegistration: true
     });
   });
 
   app
     .route('/profile')
     .get(ensureAuthenticated, (req, res) => {
-      res.render(process.cwd + '/views/pug/profile', {
+      res.render(process.cwd() + '/views/pug/profile', {
         username: req.user.username
       });
     });
@@ -71,6 +72,32 @@ myDB(async client => {
       user: req.user
     });
   });
+
+  app.route('/register')
+    .post((req, res, next) =>{
+      myDataBase.findOne({username: req.body.username}, (err, user) => {
+        if(err) {
+          next(err);
+        }else if (user){
+          res.redirect('/');
+        }else{
+          myDataBase.insertOne({
+            username: req.body.username,
+            password: req.body.password
+          }, (err, doc) => {
+            if(err){
+              res.redirect('/');
+            } else {
+              next(null, doc.ops[0]);
+            }
+          })
+        }
+      })
+    },
+    passport.authenticate('local', {failureRedirect: '/'}),
+    (req, res, next) => {
+      res.redirect('/profile');
+    });
 
   app.route('/logout')
     .get((req, res) => {
